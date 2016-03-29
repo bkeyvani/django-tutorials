3be58dfdcfbc0dfcf3e2c4381ae1cebd1f302ba7
babakkeyvani <babak.keyvani@cpcc.edu>
2016-03-29 15:40:39 -0400

    completed 'Improving our view'
    https://docs.djangoproject.com/en/1.8/intro/tutorial05/#improving-our-view

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.utils import timezone
 from django.views import generic
 
 from .models import Choice, Question
@@ -12,7 +13,9 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last 5 published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
